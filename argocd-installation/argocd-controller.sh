573bdacebb0f7c6508884f23f3d6b6723ec25697
chaithanya1812 <bob@example.com>
2024-10-10 11:02:41 +0530

    roboshop-helm frontend

--- argocd-installation/argocd-controller.sh
+++ argocd-installation/argocd-controller.sh
@@ -1,6 +1,6 @@
 # kubectl create namespace argocd
 
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+k
 
 sleep 20
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort"}}'
